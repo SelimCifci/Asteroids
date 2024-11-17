4a5a3502443e0ef2c4f089912003d4062d122dd3
SelimCifci <cifcihasanselim@gmail.com>
2024-11-17 11:36:45 +0100

    added generation of obstacles and increasing difficulty

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import pygame
-import player
 import obstacle
 
 pygame.init()
 
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((600,600))
 
 clock = pygame.time.Clock()
-d=0
+d = 0
+frames = 0
 
-asteroid = obstacle.Asteroid(0,0,1,1,20,20,100,screen)
-asteroid.normalize()
+frames_per_spawn = 60
+obstacle_speed = 100
+
+asteroids = []
 
 running = True
 while running:
@@ -20,10 +22,24 @@ while running:
 
     screen.fill((0,0,0))
 
-    asteroid.update(d/1000)
-    asteroid.draw()
+    frames += 1
+
+    if frames_per_spawn >= 22 and frames % 300 == 0:
+        frames_per_spawn -= 2
+        obstacle_speed += 5
+
+    if len(asteroids) < 24:
+        if frames % frames_per_spawn == 0:
+            asteroids.append(obstacle.Asteroid(obstacle_speed, screen))
+            asteroids[len(asteroids)-1].normalize()
+    else: asteroids.pop(0)
+
+    for i in asteroids:
+        i.update(d/1000)
+        i.draw()
 
     pygame.display.flip()
+    
     d = clock.tick(60)
 
 pygame.quit()
--- obstacle.py
+++ obstacle.py
@@ -1,29 +1,54 @@
-from dataclasses import dataclass
 import pygame
+import random
 
-@dataclass
 class Asteroid:
-    x: float
-    y: float
-    dirx: float
-    diry: float
-    sizex: int
-    sizey: int
-    speed: float
-    screen: pygame.Surface
+    def __init__(self, speed, screen) -> None:
+        size = random.randint(16,32)
+        self.size = (size, size)
+        self.speed = speed
+        self.screen = screen
+        self.pos = ()
+        self.dir = ()
 
-    def normalize(self):
-        norm = (self.dirx**2 + self.diry**2)**0.5
-        self.dirx /= norm
-        self.diry /= norm
+        spawn_pos = random.randint(0, 7)
+        match spawn_pos:
+            case 0:
+                self.pos = (random.randint(300,600),0-size)
+                self.dir = (random.uniform(-0.9,0.1),random.uniform(0.1,0.9))
+            case 1:
+                self.pos = (600,random.randint(0-size,300-size))
+                self.dir = (random.uniform(-0.9,0.1),random.uniform(0.1,0.9))
+            case 2:
+                self.pos = (600,random.randint(300,600))
+                self.dir = (random.uniform(-0.9,0.1),random.uniform(-0.9,0.1))
+            case 3:
+                self.pos = (random.randint(300,600),600)
+                self.dir = (random.uniform(-0.9,0.1),random.uniform(-0.9,0.1))
+            case 4:
+                self.pos = (random.randint(0-size,300-size),600)
+                self.dir = (random.uniform(0.1,0.9),random.uniform(-0.9,0.1))
+            case 5:
+                self.pos = (0-size,random.randint(300,600))
+                self.dir = (random.uniform(0.1,0.9),random.uniform(-0.9,0.1))
+            case 6:
+                self.pos = (0-size,random.randint(0-size,300-size))
+                self.dir = (random.uniform(0.1,0.9),random.uniform(0.1,0.9))
+            case 7:
+                self.pos = (random.randint(0-size,300-size),0-size)
+                self.dir = (random.uniform(0.1,0.9),random.uniform(0.1,0.9))
+        
+        self.rect = pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+
+    def normalize(self) -> None:
+        norm = (self.dir[0]**2 + self.dir[1]**2)**0.5
+        self.dir = (self.dir[0]/norm, self.dir[1]/norm)
 
     def update(self, delta: float) -> None:
-        self.x += self.dirx * self.speed * delta
-        self.y += self.diry * self.speed * delta
+        self.rect.left += self.dir[0] * self.speed * delta
+        self.rect.top += self.dir[1] * self.speed * delta
     
     def draw(self) -> None:
-        rect = pygame.Rect(self.x-self.sizex/2, self.y-self.sizey/2, self.sizex, self.sizey)
-        pygame.draw.rect(self.screen, (0,255,0), rect)
+        pygame.draw.rect(self.screen, (0,255,0), self.rect)
 
 class Enemy:
     def __init__(self) -> None:
